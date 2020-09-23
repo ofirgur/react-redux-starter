8c1cea11f750dcc51ba704d94c863911d201b869
Ofir <ofirgu@payoneer.com>
2020-09-23 15:08:15 +0300

    update

--- src/navigations/MaterialUINav/index.js
+++ src/navigations/MaterialUINav/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
+
+import { Button } from '@material-ui/core';
+import AccessibleIcon from '@material-ui/icons/Accessible';
 
 const Modules = () => {
     return (
         <div className="material-ui-nav">           
             <Button color="primary" variant="contained">Hello World</Button>
+            <br />
+            <br />
+            <AccessibleIcon />
         </div>
     );
 };
